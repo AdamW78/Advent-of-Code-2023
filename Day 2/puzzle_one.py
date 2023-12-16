fe47b9dbab9405478047a4c327b7c8db2ca379a4
Adam Wojciak <Code.AdamW78@gmail.com>
2023-12-15 23:11:48 -0500

    Refactored puzzle_one.py and puzzle_two.py so that they do not repeat code, rather importing shared code from cubedraw.py

--- Day 2/puzzle_one.py
+++ Day 2/puzzle_one.py
@@ -1,32 +1,8 @@
-from typing import List
+from cubedraw import get_cube_draw
 
 RED_CUBES = 12
 GREEN_CUBES = 13
 BLUE_CUBES = 14
-
-
-class CubeDraw:
-    def __init__(self):
-        self.red = -1
-        self.green = -1
-        self.blue = -1
-
-    def __str__(self):
-        return f"Red Cubes: {self.red}, Green Cubes: {self.green}, Blue Cubes: {self.blue}"
-
-
-def get_cube_draw(drawn_cube_string: List[str]) -> CubeDraw:
-    return_cube_draw = CubeDraw()
-    for cube_set in drawn_cube_string:
-        red_find = cube_set.rfind("red")
-        green_find = cube_set.rfind("green")
-        if red_find > 0:
-            return_cube_draw.red = int(cube_set[:cube_set.index(" ")])
-        elif green_find > 0:
-            return_cube_draw.green = int(cube_set[:cube_set.index(" ")])
-        else:
-            return_cube_draw.blue = int(cube_set[:cube_set.index(" ")])
-    return return_cube_draw
 
 
 raw_data = []
--- Day 2/puzzle_two.py
+++ Day 2/puzzle_two.py
@@ -1,36 +1,9 @@
-import math
-from typing import List
-
-
-
-class CubeDraw:
-    def __init__(self):
-        self.red = -1
-        self.green = -1
-        self.blue = -1
-
-    def __str__(self):
-        return f"Red Cubes: {self.red}, Green Cubes: {self.green}, Blue Cubes: {self.blue}"
-
-
-def get_cube_draw(drawn_cube_string: List[str]) -> CubeDraw:
-    return_cube_draw = CubeDraw()
-    for cube_set in drawn_cube_string:
-        red_find = cube_set.rfind("red")
-        green_find = cube_set.rfind("green")
-        if red_find > 0:
-            return_cube_draw.red = int(cube_set[:cube_set.index(" ")])
-        elif green_find > 0:
-            return_cube_draw.green = int(cube_set[:cube_set.index(" ")])
-        else:
-            return_cube_draw.blue = int(cube_set[:cube_set.index(" ")])
-    return return_cube_draw
-
+from cubedraw import get_cube_draw
 
 raw_data = []
 with open('input', 'rt', encoding='utf8') as input_file:
     raw_data = input_file.readlines()
-games = len(raw_data)*[None]
+games = len(raw_data) * [None]
 for line_number, line in enumerate(raw_data):
     line = line.rstrip('\n')
     line = line[line.index(': ') + 2:]
