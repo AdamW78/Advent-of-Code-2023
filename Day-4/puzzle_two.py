014debb2d0ed7a490992c187d23c00351f6dbc94
Adam Wojciak <Code.AdamW78@gmail.com>
2023-12-17 17:35:39 -0500

    Updated Day 4 puzzles to be PyLint-compliant

--- Day-4/puzzle_two.py
+++ Day-4/puzzle_two.py
@@ -1,21 +1,20 @@
 """
 https://adventofcode.com/2023/day/4#part2
 """
-input_array = None
-with open('input', 'rt', encoding='utf8') as input_file:
-    input_array = input_file.readlines()
+from read_input import read
 
-card_totals = [0] * len(input_array)
-for line_number, line in enumerate(input_array):
+INPUT_ARR = read()
+card_totals = [0] * len(INPUT_ARR)
+for line_number, line in enumerate(INPUT_ARR):
     line = line[line.index(': ') + 2:]
     line = line.split(' | ')
     winning_numbers = [int(x) for x in line[0].split(' ') if x]
     card_numbers = [int(x) for x in line[1].split(' ') if x]
-    num_winning_numbers = 0
+    NUM_WINNING_NUMBERS = 0
     for number in card_numbers:
         if number in winning_numbers:
-            num_winning_numbers += 1
-    card_total = 2 ** (num_winning_numbers - 1)
+            NUM_WINNING_NUMBERS += 1
+    card_total = 2 ** (NUM_WINNING_NUMBERS - 1)
     if card_total >= 1:
         card_totals[line_number] = card_total
 print(sum(card_totals))
