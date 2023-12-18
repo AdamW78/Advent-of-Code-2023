ac01ce7cf3f3d5ef2e0521f662a4fa7d19f8ef85
Adam Wojciak <Code.AdamW78@gmail.com>
2023-12-17 19:25:23 -0500

    Added example_read() to read_input.py

--- read_input.py
+++ read_input.py
@@ -11,3 +11,13 @@ def read():
     with open('input', 'rt', encoding='utf8') as input_file:
         input_arr = input_file.readlines()
         return input_arr
+
+
+def example_read():
+    """
+    Reads example input, returns an array of strings (lines)
+    :return: list of strings (lines)
+    """
+    with open('example_input', 'rt', encoding='utf8') as example_input_file:
+        example_input_arr = example_input_file.readlines()
+        return example_input_arr
