32438a27f44bbd3dc9e322f9001863c952053649
Adam Wojciak <Code.AdamW78@gmail.com>
2023-12-19 21:21:25 -0500

    Solved Day 5 puzzle_one.py

--- Day-5/puzzle_one.py
+++ Day-5/puzzle_one.py
@@ -3,11 +3,10 @@ https://adventofcode.com/2023/day/5#day-desc
 """
 from collections import namedtuple
 from io import TextIOWrapper
-from typing import List
-
+from typing import List, Tuple
 
 NUM_MAPPINGS = 7
-Mapping = namedtuple('Mapping', 'start end mapping')
+Mapping = namedtuple('Mapping', 'start end mapping ')
 
 
 def read_mappings(mapping_int: int, mappings: List[List[Mapping]], inp: TextIOWrapper):
@@ -20,7 +19,11 @@ def read_mappings(mapping_int: int, mappings: List[List[Mapping]], inp: TextIOWr
     """
     mapping = inp.readline()
     while mapping and mapping != '\n':
-        mappings[mapping_int].append(Mapping(*[int(x) for x in mapping.rstrip('\n').split(' ')]))
+        mapping = [int(y) for y in mapping.rstrip('\n').split(' ')]
+        start = mapping[1]
+        end = start + mapping[2]
+        dest = mapping[0]
+        mappings[mapping_int].append(Mapping(start, end, dest))
         mapping = inp.readline()
 
 
@@ -36,18 +39,45 @@ def parse_category_mapping(inp: TextIOWrapper, category_mappings: List[List[Mapp
         inp.readline()
 
 
-def line_by_line_read():
+def line_by_line_read() -> Tuple[Tuple[int, ...], Tuple[List[Mapping], ...]]:
     """
-    Reads the input file, one line at a time
+    Reads the input file line-by-line, creating mappings according it
+    :return: Seeds to map, and the mappings to use
     """
-    with open('example_input', 'rt', encoding='utf8') as file_input:
+    with open('input', 'rt', encoding='utf8') as file_input:
         line = file_input.readline()
+        seeds = tuple(int(seed) for seed in line.strip('seeds: ').rstrip('\n').split(' '))
+        category_mappings = tuple([] for _ in range(NUM_MAPPINGS))
+        # READ NEWLINE
         file_input.readline()
-        seeds = line.strip('seeds: ').rstrip('\n').split(' ')
-        seeds = tuple(int(seed) for seed in seeds)
-        category_mappings = [[] for _ in range(NUM_MAPPINGS)]
         parse_category_mapping(file_input, category_mappings)
-        print(category_mappings)
+        return seeds, category_mappings
 
 
-line_by_line_read()
+def convert_seeds_to_locations(seeds, category_mappings) -> List[int]:
+    """
+    Iterates through each set of mappings in category_mappings
+    It updates seeds to soil, soil to fertilizer, etc.
+    :param seeds: seeds to update to soil, etc.
+    :param category_mappings: sets of mappings used to convert seeds to soil, etc.
+    :return: seeds converted to locations, in a list
+    """
+    seeds = list(seeds)
+    mapped_vals = [-1 for _ in range(len(seeds))]
+    for mapping_list in category_mappings:
+        for seed_num, seed in enumerate(seeds):
+            i = 0
+            while mapped_vals[seed_num] == -1 and i < len(mapping_list):
+                if mapping_list[i].start <= seed < mapping_list[i].end:
+                    mapped_vals[seed_num] = mapping_list[i].mapping + (seed - mapping_list[i].start)
+                i += 1
+        for vi, mapped_val in enumerate(mapped_vals):
+            if mapped_val != -1:
+                seeds[vi] = mapped_val
+                mapped_vals[vi] = -1
+    return seeds
+
+
+x = line_by_line_read()
+locations = convert_seeds_to_locations(x[0], x[1])
+print(min(locations))
