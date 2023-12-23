5e84f66822ebaa2f2b3b15619a088723499f3f0a
Adam Wojciak <Code.AdamW78@gmail.com>
2023-12-23 17:52:46 -0500

    Solved Day 7 puzzle_two.py

--- Day-7/camel_cards.py
+++ Day-7/camel_cards.py
@@ -7,17 +7,60 @@ from typing import List
 
 Hand = namedtuple('Hand', 'cards bet type')
 
-FIVE_OF_KIND = 6
-FOUR_OF_KIND = 5
-FULL_HOUSE = 4
+FIVE_OF_KIND = 0
+FOUR_OF_KIND = 1
+FULL_HOUSE = 2
 THREE_OF_KIND = 3
-TWO_PAIR = 2
-ONE_PAIR = 1
-HIGH_CARD = 0
-CARD_SUITS = ('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0')
+TWO_PAIR = 4
+ONE_PAIR = 5
+HIGH_CARD = 6
+CARD_SUITS_JACK = ('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+CARD_SUITS_JOKER = ('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
 
 
-def get_hand_type(cards: str) -> int:  # pylint: disable=too-many-return-statements
+def compare_suits_joker(hand_a: Hand, hand_b: Hand) -> int:
+    """
+    Comparison function used to compare the suits of the cards between two hands
+    :param hand_a: The first hand to have its cards' suits compared.
+    :param hand_b: The second hand to have its cards' suits compared.
+    :return: Zero if the suits are the same, -1 if suit A beats suit B, 1 otherwise.
+    """
+    suit_a = hand_a.cards
+    suit_b = hand_b.cards
+    if suit_a == suit_b:
+        return 0
+    for i, _ in enumerate(suit_a):
+        suit_a_index = CARD_SUITS_JOKER.index(suit_a[i])
+        suit_b_index = CARD_SUITS_JOKER.index(suit_b[i])
+        if suit_a_index > suit_b_index:
+            return 1
+        if suit_a_index < suit_b_index:
+            return -1
+    sys.exit(f"Failed to compare two cards: {hand_a} and {hand_b}.")
+
+
+def compare_suits_jack(hand_a: Hand, hand_b: Hand) -> int:
+    """
+    Comparison function used to compare the suits of the cards between two hands
+    :param hand_a: The first hand to have its cards' suits compared.
+    :param hand_b: The second hand to have its cards' suits compared.
+    :return: Zero if the suits are the same, -1 if suit A beats suit B, 1 otherwise.
+    """
+    suit_a = hand_a.cards
+    suit_b = hand_b.cards
+    if suit_a == suit_b:
+        return 0
+    for i, _ in enumerate(suit_a):
+        suit_a_index = CARD_SUITS_JACK.index(suit_a[i])
+        suit_b_index = CARD_SUITS_JACK.index(suit_b[i])
+        if suit_a_index > suit_b_index:
+            return 1
+        if suit_a_index < suit_b_index:
+            return -1
+    sys.exit(f"Failed to compare two cards: {hand_a} and {hand_b}.")
+
+
+def get_hand_type(cards: str, jokers=False) -> int:  # pylint: disable=too-many-return-statements
     """
     Returns the type of hand that a given set of cards is
     :param cards: A string of five card suits (characters)
@@ -30,6 +73,21 @@ def get_hand_type(cards: str) -> int:  # pylint: disable=too-many-return-stateme
         else:
             card_nums[card] += 1
     max_card_num = max(card_nums.values())
+    if jokers and 'J' in card_nums:
+        if card_nums['J'] == 5:
+            return FIVE_OF_KIND
+        num_jokers = card_nums['J']
+        if num_jokers == max_card_num:
+            del card_nums['J']
+            max_card_num = max(card_nums.values())
+        max_card_num += num_jokers
+        replace_max = max(card_nums.values())
+        most_freq_card = None
+        for card, num_apps in card_nums.items():
+            if num_apps == replace_max:
+                most_freq_card = card
+        card_nums[most_freq_card] = max_card_num
+
     match max_card_num:
         case 5:
             return FIVE_OF_KIND
@@ -51,28 +109,7 @@ def get_hand_type(cards: str) -> int:  # pylint: disable=too-many-return-stateme
             return HIGH_CARD
 
 
-def compare_suits(hand_a: Hand, hand_b: Hand) -> int:
-    """
-    Comparison function used to compare the suits of the cards between two hands
-    :param hand_a: The first hand to have its cards' suits compared.
-    :param hand_b: The second hand to have its cards' suits compared.
-    :return: Zero if the suits are the same, -1 if suit A beats suit B, 1 otherwise.
-    """
-    suit_a = hand_a.cards
-    suit_b = hand_b.cards
-    if suit_a == suit_b:
-        return 0
-    for i, _ in enumerate(suit_a):
-        suit_a_index = CARD_SUITS.index(suit_a[i])
-        suit_b_index = CARD_SUITS.index(suit_b[i])
-        if suit_a_index > suit_b_index:
-            return 1
-        if suit_a_index < suit_b_index:
-            return -1
-    sys.exit(f"Failed to compare two cards: {hand_a} and {hand_b}.")
-
-
-def get_hands(file='example_input') -> List[Hand]:
+def get_hands(file='example_input', jokers=False) -> List[Hand]:
     """
     Read in input to namedtuple Hand containing a string cards and an int bet
     :param file: input file to read hands from, defaults to 'example_input'
@@ -84,6 +121,9 @@ def get_hands(file='example_input') -> List[Hand]:
         while line:
             line = line.strip('\n').split(' ')
             line[1] = int(line[1])
-            hands.append(Hand(line[0], line[1], get_hand_type(line[0])))
+            hand_type = get_hand_type(line[0])
+            if jokers:
+                hand_type = get_hand_type(line[0], jokers)
+            hands.append(Hand(line[0], line[1], hand_type))
             line = inp.readline()
     return hands
--- Day-7/puzzle_two.py
+++ Day-7/puzzle_two.py
@@ -1,10 +1,11 @@
+
 """
-https://adventofcode.com/2023/day/7#day-desc
+https://adventofcode.com/2023/day/7#part2
 """
 from functools import cmp_to_key
 from operator import attrgetter
 
-from camel_cards import get_hands, compare_suits_jack
+from camel_cards import get_hands, compare_suits_joker
 
 
 def find_total_winnings() -> int:
@@ -13,16 +14,20 @@ def find_total_winnings() -> int:
     :return: Total winnings, as an integer
     """
     winnings = 0
-    hands = get_hands('input')
+    hands = get_hands('input', jokers=True)
     # Initial sort - based on ranking of each card in each hand
     # First card is the most important one
     # K is before Q, Q is before J, J is before T, etc.
-    hands.sort(key=cmp_to_key(compare_suits_jack))
+    hands.sort(key=cmp_to_key(compare_suits_joker))
+    print(hands)
     # Maintaining the above order (wherever possible), sort based on type of hand
     hands.sort(key=attrgetter('type'))
+    print(hands)
     rank = len(hands)
     for hand in hands:
         winnings += rank * hand.bet
+        print(f"{hand}: Rank: {rank}, Bet: {hand.bet}, Winnings: {rank * hand.bet}")
+        print(f"Total Winnings: {winnings}")
         rank -= 1
     return winnings
 
